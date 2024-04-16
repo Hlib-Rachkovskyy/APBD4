10bab0f9e7b2c5edb6b173decd987186f3ac8ab9
Hlib Rachkovskyy <s28382@pjwstk.edu.pl>
2024-04-16 20:04:40 +0200

    Two controller APIs was made

---
+++ Animal.cs
@@ -0,0 +1,12 @@
+﻿namespace APBD4;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public double Mass { get; set; }
+    public string CoatColor { get; set; }
+
+    
+}
---
+++ Controllers/AnimalController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APBD4.Controllers;
+
+
+[Route("api/animals")]
+[ApiController]
+public class AnimalController : ControllerBase
+{
+
+    private static readonly List<Animal> _animals = new()
+    {
+        new Animal() { Id = 1, Name = "John", Category = "Pies", Mass = 3.5, CoatColor = "Purple"}, 
+        new Animal() { Id = 2, Name = "Mickey", Category = "Mouse", Mass = 0.8, CoatColor = "Black"},
+        new Animal() { Id = 3, Name = "Daniel", Category = "Monkey", Mass = 5, CoatColor = "Brown"}
+
+    };
+        
+        
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        return Ok(_animals);
+    }
+    
+    [HttpGet("id:int")]
+    public IActionResult getAnimals(int id)
+    {
+        var animal = _animals.FirstOrDefault(a => a.Id == id);
+
+        if (animal == null)
+        {
+            return NotFound($"Animal with id {id} was not found");
+        }
+        
+        return Ok(animal);
+    }
+    
+}
